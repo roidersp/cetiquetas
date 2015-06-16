2abd05ca1d53aa5409b34c6aaf518938933de7d9
Rodrigo Luis Santander <roidersp@gmail.com>
2015-06-16 12:10:05 -0500

    gtiuy

--- js/indepth.js
+++ js/indepth.js
@@ -14,23 +14,18 @@ $(".indepth_detalle_bar").on("click",function(){
 )
 
 $.getJSON( urlIndepth+"js/equipos.json", function( equipos ) {
-	console.log(equipos);
 	
 	$.getJSON( urlIndepth+"js/tenis.json", function( tenis ) {
 		
-		console.log(equipos);
+		
 		
 		
 		
 		if($(".indepth_goleadores_item").attr("href")=="#"){
 			$(".indepth_goleadores_item .indepth_loquiero").css("display","none");
 		$(".indepth_goleadores_item").bind('click', function(e){
-			
-				
 				
 				e.preventDefault();
-			
-					        
 		});
 		}
 		
@@ -44,13 +39,11 @@ $.getJSON( urlIndepth+"js/equipos.json", function( equipos ) {
 				var m_tenis=tenis["tenis"][data.attr("tenis")];
 				var m_equipo;
 				$.each(equipos["equipos"], function(index, obj){
-					console.log(obj);
 					if(index == normalize(data.attr("pais")).toLowerCase()){
 						m_equipo = obj;
 					}
 				});
 				
-				console.log(normalize(data.attr("pais")));
 				cont.find(".indepth_goleadores_tenis").html(m_tenis["marca"]+" - "+m_tenis["nombre"]);
 				
 				cont.find("#indepth_goleadores_local .indepth_goleadores_img_item").html('<img src="'+urlIndepth+'images/Camisetas/Casa/'+normalize(data.attr("pais"))+'.jpg">');
@@ -126,14 +119,8 @@ $.getJSON( urlIndepth+"js/equipos.json", function( equipos ) {
 			
 			$.each(tenis_orden, function( i, item ) {
 				
-				//console.log(tenis_orden[item["id"]]);
-				console.log(item);
 				var tenis_id=item["id"];
-				var tenis_goles=item["goles"];
-				
-				console.log(tenis_id);
-				
-				console.log(tenis_x[tenis_id]);
+				var tenis_goles=item["goles"];				
 				
 				ranking.append('<a target="_blank" class="indepth_ranking_item" href="'+tenis_x[tenis_id]["link"]+'">');
 				
